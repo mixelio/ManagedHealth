246e8627baa55c585490efd81fa4f5f71d9a813a
mixelio <mixelios@gmail.com>
2022-02-14 11:28:48 +0200

    2

--- js/script.js
+++ js/script.js
@@ -21,3 +21,14 @@ new Swiper(".motivation__swiper", {
     hideOnClick: !0
   }
 });
+
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".burger__menu");
+
+if (iconMenu) {
+  iconMenu.addEventListener("click", function(e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
+}
